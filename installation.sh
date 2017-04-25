41272f0769f3da9ca18292afd63d202772413f0e
Žilvinas Navickas <navickaszilvinas@gmail.com>
2017-04-25 23:42:51 +0300

    fix installation sourcing

--- installation.sh
+++ installation.sh
@@ -8,7 +8,7 @@ zDictCompletionDir=/etc/bash_completion.d
 cp dictionary $zDictScriptDir 
 cp dictionary_autocomplete $zDictCompletionDir
 
-. "$zDictScriptDir/dictionary"
-. "$zDictCompletionDir/dictionary_autocomplete" 
+. ${zDictScriptDir}"/dictionary"
+. ${zDictCompletionDir}"/dictionary_autocomplete" 
 
 #Add sourcing to bashrc if you like to use it
